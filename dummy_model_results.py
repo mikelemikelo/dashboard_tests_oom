46eeafb73e63e6af03e568b3afe88ec025de7807
Miguel Rosas <mikelemikelo@gmail.com>
2022-09-20 12:06:11 -0600

    Pushing really small MTRs, around 180KB

--- dummy_model_results.py
+++ dummy_model_results.py
@@ -4,13 +4,15 @@ import json
 def metrics(baseline, comparator) -> dict:
 
 	jsonResult = loadJsonFromFilename("dummy_base_mtr.json")
-	additionalData = loadJsonFromFilename("dummy_data_empty.json")
-	jsonResult.update(additionalData)
+	# additionalData = loadJsonFromFilename("dummy_data_empty.json")
+	# jsonResult.update(additionalData)
 	# additionalData = loadJsonFromFilename("dummy_data_3MB.json")
 	# jsonResult.update(additionalData)
 	# additionalData = loadJsonFromFilename("dummy_data_1MB.json")
 	# jsonResult.update(additionalData)
-	additionalData = loadJsonFromFilename("dummy_data_2MB.json")
+	# additionalData = loadJsonFromFilename("dummy_data_2MB.json")
+	# jsonResult.update(additionalData)
+	additionalData = loadJsonFromFilename("dummy_data_50KB.json")
 	jsonResult.update(additionalData)
 	yield jsonResult
 
